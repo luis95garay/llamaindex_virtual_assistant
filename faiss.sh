e6bb8ddcccce682331fd4380b310b79eb6fb16dc
Luis Fernando González Garay <luis95.garay@gmail.com>
2023-09-09 20:03:52 -0600

    clean an delete files

--- faiss.sh
+++
@@ -1,2 +0,0 @@
-cd C:/Users/"Luis Fernando"/venv/chat/Lib/site-packages/faiss
-ln -s swigfaiss.py swigfaiss_avx2.py
---
+++ run.sh
@@ -0,0 +1,5 @@
+docker run \
+        -it \
+        -p 9000:9000 \
+        -v ~/langchain_virtual_assistant/:/home/app \
+        langchain-assistant-v0
